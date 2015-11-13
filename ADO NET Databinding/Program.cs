2046b1944967414b23a1151d7220cac0d168534d
lester88a <lester88a@gmail.com>
2015-11-13 15:34:24 -0500

    Create relationships for 3 tables

--- ADO NET Databinding/Program.cs
+++ ADO NET Databinding/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,12 @@ namespace ADO_NET_Databinding
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
             //ADO.Net DATA BINDING
+            //data binding model
+            //Synchronized
 
         }
     }
